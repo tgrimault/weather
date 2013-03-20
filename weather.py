cf57ae7b9870f4200a8cc72715e0cc482271f93e
Thomas <t.grimault@laposte.net>
2013-03-19 23:23:46 -0400

    Basic fetch and display info from openweathermap.org

---
+++ weather.py
@@ -0,0 +1,11 @@
+import json
+from urllib2 import urlopen
+from pprint import pprint
+
+data = urlopen('http://api.openweathermap.org/data/2.1/forecast/city?q=Beloeil,qc&units=metric')
+forcasts = json.load(data)
+for forcast in forcasts['list']:
+	pprint(forcast['dt_txt'])
+	pprint(forcast['main'])
+	pprint(forcast['weather'])
+
