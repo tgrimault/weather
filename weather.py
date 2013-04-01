f1879fe2db43566339a83335e156d4f8e0a8543f
Thomas <t.grimault@laposte.net>
2013-03-31 23:19:29 -0400

    move all the the weather fetching stuff into a dedicated object.

--- weather.py
+++ weather.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 import json
 from urllib2 import urlopen
-from pprint import pprint
 
-data = urlopen('http://api.openweathermap.org/data/2.1/forecast/city?q=Beloeil,qc&units=metric')
-forecasts = json.load(data)
-for forecast in forecasts['list']:
-	full_date = forecast['dt_txt'].split(' ')
-	date = full_date[0]
-	time = full_date[1]
-	temp = forecast['main']['temp']
-	weather = forecast['weather'][0]['main']
-	print date
-	print time
-	print temp
-	print weather
-	separator = "*************************************"
+class WForecast:
+	date = []
+	time = []
+	temp = []
+	weather = []
+	nbOfForecast = 0
+
+	def __init__(self):
+		data = urlopen('http://api.openweathermap.org/data/2.1/forecast/city?q=Beloeil,qc&units=metric')
+		forecasts = json.load(data)
+		for forecast in forecasts['list']:
+			full_date = forecast['dt_txt'].split(' ')
+			self.date.append(full_date[0])
+			self.time.append(full_date[1])
+			self.temp.append(forecast['main']['temp'])
+			self.weather.append(forecast['weather'][0]['main'])
+			self.nbOfForecast += 1
+
+	def __getitem__(self):
+		return self
+
+
+
+forecast = WForecast()
+separator = "*************************************"
+
+for i in range(0, forecast.nbOfForecast):
+	print forecast.date[i]
+	print forecast.time[i]
+	print forecast.temp[i]
+	print forecast.weather[i]
 	print separator
 
