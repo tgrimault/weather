e4d1e1d8caf13b31d14f31e251a05af84b2d51e2
Thomas <t.grimault@laposte.net>
2013-03-31 21:35:21 -0400

    parse the openweathermap.org response to extract the date&time, the forecast temperature and weather.

--- weather.py
+++ weather.py
@@ -4,9 +4,17 @@ from urllib2 import urlopen
 from pprint import pprint
 
 data = urlopen('http://api.openweathermap.org/data/2.1/forecast/city?q=Beloeil,qc&units=metric')
-forcasts = json.load(data)
-for forcast in forcasts['list']:
-	pprint(forcast['dt_txt'])
-	pprint(forcast['main'])
-	pprint(forcast['weather'])
+forecasts = json.load(data)
+for forecast in forecasts['list']:
+	full_date = forecast['dt_txt'].split(' ')
+	date = full_date[0]
+	time = full_date[1]
+	temp = forecast['main']['temp']
+	weather = forecast['weather'][0]['main']
+	print date
+	print time
+	print temp
+	print weather
+	separator = "*************************************"
+	print separator
 
